6201c2828338584be39d104f4073215eb8f1c4ed
Robin Lovelace <rob00x@gmail.com>
2018-11-22 15:44:03 +0000

    Add schema

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Stats19 schema
+#'
+#' @docType data
+#' @keywords datasets
+#' @rdname read_schema
+#' @name stats19_schema
+NULL
+# Example stats19 data file
+#
+# The data was created using the xcv rust program (see vignette for details).
+# The raw csv file is stored as `ac_2005_2014_100.csv`
+# @rdname ac_2005_2014_100
+# "ac_2005_2014_100"
+
