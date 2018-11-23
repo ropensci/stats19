a2c596904d28c75a5588506e38bcd0b048cf4b6f
Layik <layik.hama@gmail.com>
2018-11-23 16:04:51 +0000

    Codecov

--- tests/testthat/test-dl_stats19.R
+++ tests/testthat/test-dl_stats19.R
@@ -3,3 +3,16 @@ context("test-dl_stats19")
 test_that("dl_stats19_2017_ac works", {
   expect_output(dl_stats19_2017_ac())
 })
+
+test_that("dl_stats19 works for junk", {
+  expect_output(dl_stats19(type = "junk"))
+})
+
+test_that("dl_stats19 works for default", {
+  expect_output(dl_stats19())
+})
+
+test_that("dl_stats19 works for chosen file name", {
+  expect_output(dl_stats19(
+    file_name = stats19::file_names$DfTRoadSafety_Accidents_2009.zip))
+})
