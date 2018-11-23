f6485c5066eef551982c9e33dc8affa8a2e22a02
Layik <layik.hama@gmail.com>
2018-11-23 17:03:09 +0000

    More coverage

--- tests/testthat/test-dl_stats19.R
+++ tests/testthat/test-dl_stats19.R
@@ -12,6 +12,10 @@ test_that("dl_stats19 works for default", {
   expect_output(dl_stats19())
 })
 
+test_that("dl_stats19 works for 2017", {
+  expect_output(dl_stats19(years = "2017"))
+})
+
 test_that("dl_stats19 works for chosen file name", {
   expect_output(dl_stats19(
     file_name = stats19::file_names$DfTRoadSafety_Accidents_2009.zip))
