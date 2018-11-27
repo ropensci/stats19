c0e30261cecc5415e0b91c6ab74931055a31294b
Layik <layik.hama@gmail.com>
2018-11-27 11:57:11 +0000

    Skip download tests on no internet connection using curl package

--- tests/testthat/test-format.R
+++ tests/testthat/test-format.R
@@ -1,6 +1,15 @@
 context("test-format")
 
+# To run download functions you need an internet connection.
+# pref a fast one
+skip_download = function() {
+  connected = !is.null(curl::nslookup("r-project.org", error = FALSE))
+  if(!connected)
+    skip("No connection to run download function.")
+}
+
 test_that("read_schema works", {
+  skip_download()
   t = stats19::read_schema()
   expect_equal(nrow(t), 969)
 })
--- tests/testthat/test-read_stats19.R
+++ tests/testthat/test-read_stats19.R
@@ -1,6 +1,14 @@
 context("test-read_stats19")
+# To run download functions you need an internet connection.
+# pref a fast one
+skip_download = function() {
+  connected = !is.null(curl::nslookup("r-project.org", error = FALSE))
+  if(!connected)
+    skip("No connection to run download function.")
+}
 
 test_that("read_stats19 works", {
+  skip_download()
   # download real data
   acc_2016 = file_names$dftRoadSafety_Accidents_2016.zip
   dl_stats19(file_name = acc_2016)
