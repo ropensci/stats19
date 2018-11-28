43ab452c03766ecdd20181ed769ed53d942d47a4
Layik <layik.hama@gmail.com>
2018-11-28 10:38:39 +0000

    Clean up handy dl_ functions, see #12

--- tests/testthat/test-dl_stats19.R
+++ tests/testthat/test-dl_stats19.R
@@ -1,10 +1,5 @@
 context("test-dl_stats19")
 
-test_that("dl_2017_accidents works", {
-  skip_download()
-  expect_output(dl_2017_accidents())
-})
-
 test_that("dl_stats19 works for junk", {
   expect_output(dl_stats19(type = "junk"))
   expect_output(dl_stats19(years = "2999", type = "junk"))
