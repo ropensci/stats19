dc5ca8e18aaaae32d606d97363a8f6b16ab8c40b
Layik <layik.hama@gmail.com>
2018-11-30 12:29:18 +0000

    Minor, rhub check 1, #17

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,7 +1,10 @@
 context("test-utils")
 
 test_that("geturl works", {
-  expect_equal(get_url(), "http://data.dft.gov.uk.s3.amazonaws.com/road-accidents-safety-data/")
+  expect_equal(get_url(),
+               file.path("http://data.dft.gov.uk.s3.amazonaws.com",
+                         "road-accidents-safety-data/")
+               )
 })
 
 test_that("find_file_name works", {
