9e953bd0a209311f71d726741a1840b3ff371219
Robin Lovelace <rob00x@gmail.com>
2018-12-04 15:00:36 +0000

    Close #27

---
+++ R/stats19-package.R
@@ -0,0 +1,9 @@
+.onAttach = function(libname, pkgname) {
+  msg = paste0 (
+      "Data provided under the conditions of the Open Government License.\n",
+      "If you use data from this package, ",
+      "mention the source\n(Department for Transport), cite the package and link to:\n",
+      "www.nationalarchives.gov.uk/doc/open-government-licence/version/3/."
+    )
+  packageStartupMessage (msg)
+}
