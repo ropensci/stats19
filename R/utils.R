326d69b1114a7f1ef958d93d8f0f749f2705eb7a
Robin Lovelace <rob00x@gmail.com>
2018-12-30 00:43:02 +0000

    Tidy code - remove whitespace

--- R/utils.R
+++ R/utils.R
@@ -94,7 +94,7 @@ find_file_name = function(years = NULL, type = NULL) {
       }
     }
   }
-  if (any(grepl("Stats19-Data1979-2004.zip", result))) {
+  if(any(grepl("Stats19-Data1979-2004.zip", result))) {
     # extra warnings
     message("\033[31mThis will download 240 MB+ (1.8 GB unzipped).\033[39m")
     message("Coordinates unreliable in this data.")
