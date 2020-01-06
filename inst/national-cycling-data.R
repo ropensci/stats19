6f86a92d4c87c9f769cff9ed8defd6d69ff5a8a7
Robin Lovelace <rob00x@gmail.com>
2020-01-06 16:27:29 +0000

    Close #138

--- inst/national-cycling-data.R
+++ inst/national-cycling-data.R
@@ -47,3 +47,35 @@ cav_min %>% select(age_of_casualty, age_of_driver, casualty_imd_decile, driver_i
 readr::write_csv(cav_min, "casualties_cyclist_other_2_vehicles.csv")
 piggyback::pb_upload("casualties_cyclist_other_2_vehicles.csv")
 piggyback::pb_download_url("casualties_cyclist_other_2_vehicles.csv")
+
+
+# add imd scores ----------------------------------------------------------
+
+u = "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/833970/File_1_-_IMD2019_Index_of_Multiple_Deprivation.xlsx"
+download.file(u, "File_1_-_IMD2019_Index_of_Multiple_Deprivation.xlsx")
+imd_scores = readxl::read_excel("File_1_-_IMD2019_Index_of_Multiple_Deprivation.xlsx", sheet = 2)
+imd_scores_min = imd_scores %>%
+  select(geo_code = `LSOA code (2011)`, `Index of Multiple Deprivation (IMD) Decile`)
+lsoas = sf::read_sf("https://github.com/npct/pct-outputs-national/raw/master/commute/lsoa/z_all.geojson")
+summary(lsoas$geo_code %in% imd_scores$`LSOA code (2011)`)
+lsoas_min = lsoas %>% select(geo_code, lad_name, all, bicycle, car_driver)
+lsoas_imd = left_join(lsoas_min, imd_scores_min)
+sf::write_sf(lsoas_imd, "lsoa_zones_imd_2019.geojson")
+piggyback::pb_upload("lsoa_zones_imd_2019.geojson")
+lsoas_imd_min = lsoas_imd %>% select(imd_decile_2019 = `Index of Multiple Deprivation (IMD) Decile`, lad_name)
+plot(lsoas_imd_min[lsoas_imd_min$lad_name == "Leeds", ])
+
+cav_sf = cav_min %>%
+  filter(!is.na(latitude)) %>%
+  sf::st_as_sf(., coords = c("longitude", "latitude"), crs = 4326)
+plot(cav_sf[1:999, "speed_limit"])
+
+cav_imd = sf::st_join(cav_sf, lsoas_imd_min)
+names(cav_imd)
+plot(cav_imd[1:999, "lad_name"])
+plot(cav_imd[1:999, "imd_decile_2019"])
+
+cav_imd_df = cav_imd %>% sf::st_drop_geometry()
+readr::write_csv(cav_imd_df, "casualties_cyclist_other_2_vehicles_imd.csv")
+piggyback::pb_upload("casualties_cyclist_other_2_vehicles_imd.csv")
+piggyback::pb_download_url("casualties_cyclist_other_2_vehicles_imd.csv")
