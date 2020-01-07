dc62ae8cf1a2c52a5be07711330905d8e89e1aeb
Robin Lovelace <rob00x@gmail.com>
2020-01-07 07:47:30 +0000

    Update style in iow example

--- inst/iow_example.R
+++ inst/iow_example.R
@@ -8,18 +8,18 @@ library(igraph)
 library(purrr)
 
 # download geofabric data for IOW
-iow_geofabric <- readRDS(url("https://github.com/ropensci/stats19/releases/download/1.1.0/roads_key.Rds")) %>%
+iow_geofabric = readRDS(url("https://github.com/ropensci/stats19/releases/download/1.1.0/roads_key.Rds")) %>%
   st_transform(crs = 27700)
-iow_bb <- getbb("Isle of Wight, South East, England", format_out = "sf_polygon", featuretype = "state") %>%
+iow_bb = getbb("Isle of Wight, South East, England", format_out = "sf_polygon", featuretype = "state") %>%
   st_transform(crs = 27700)
 
 # filter only certain types of highways
 key_roads_text = "primary|secondary|tertiary"
-iow_geofabric <- iow_geofabric %>%
+iow_geofabric = iow_geofabric %>%
   filter(grepl(pattern = key_roads_text, x = fclass))
 
 # fix link labels
-iow_geofabric <- iow_geofabric %>%
+iow_geofabric = iow_geofabric %>%
   mutate(highway = sub("_link", "", fclass))
 
 # plot
@@ -37,14 +37,14 @@ tm_shape(iow_bb, ext = 1.05) +
   tm_scale_bar()
 
 # download car crashes data and transform in sf
-car_accidents_2017 <- get_stats19(year = 2018)
-car_accidents_2017 <- car_accidents_2017 %>%
+car_accidents_2017 = get_stats19(year = 2018)
+car_accidents_2017 = car_accidents_2017 %>%
   filter(!is.na(longitude), !is.na(latitude)) %>% # NA in coordinates
   st_as_sf(coords = c("longitude", "latitude"), crs = 4326) %>%
   st_transform(crs = 27700)
 
 # filter car crashes in IOW
-car_accidents_2017_iow <- car_accidents_2017[iow_bb, ]
+car_accidents_2017_iow = car_accidents_2017[iow_bb, ]
 
 # plot
 tm_shape(iow_bb, ext = 1.05) +
@@ -64,7 +64,7 @@ tm_shape(iow_bb, ext = 1.05) +
 
 
 # Exclude car crashes far from all streets
-car_accidents_2017_iow <- car_accidents_2017_iow[iow_geofabric,
+car_accidents_2017_iow = car_accidents_2017_iow[iow_geofabric,
   op = st_is_within_distance, dist = units::set_units(50, "m")
 ]
 
@@ -85,11 +85,11 @@ tm_shape(iow_bb, ext = 1.05) +
   tm_scale_bar()
 
 # match each car crashes with its nearest highway
-ID_nearest_highway <- st_nearest_feature(car_accidents_2017_iow, iow_geofabric)
-number_of_car_accidents <- factor(ID_nearest_highway, levels = seq_len(nrow(iow_geofabric))) %>%
+ID_nearest_highway = st_nearest_feature(car_accidents_2017_iow, iow_geofabric)
+number_of_car_accidents = factor(ID_nearest_highway, levels = seq_len(nrow(iow_geofabric))) %>%
   table() %>% as.numeric()
 
-iow_geofabric <- iow_geofabric %>%
+iow_geofabric = iow_geofabric %>%
   mutate(number_of_car_accidents = as.character(number_of_car_accidents))
 
 # plot
@@ -108,14 +108,14 @@ tm_shape(iow_bb) +
 # problems...
 
 # spatial smoothing (like moving average filtering)
-iow_graph <- st_touches(iow_geofabric) %>% graph.adjlist()
-iow_graph_ego <- ego(iow_graph, order = 2)
+iow_graph = st_touches(iow_geofabric) %>% graph.adjlist()
+iow_graph_ego = ego(iow_graph, order = 2)
 
-spatial_smoothing <- function(ID) {
+spatial_smoothing = function(ID) {
   mean(number_of_car_accidents[iow_graph_ego[[ID]]])
 }
 
-iow_geofabric <- iow_geofabric %>%
+iow_geofabric = iow_geofabric %>%
   mutate(number_of_car_accidents_smooth = map_dbl(seq_len(nrow(.)), spatial_smoothing))
 
 # plot
@@ -134,9 +134,9 @@ tm_shape(iow_bb, ext = 1.05) +
   tm_scale_bar()
 
 # works. let's tune it!
-iow_graph_ego <- ego(iow_graph, order = 4)
+iow_graph_ego = ego(iow_graph, order = 4)
 
-iow_geofabric <- iow_geofabric %>%
+iow_geofabric = iow_geofabric %>%
   mutate(number_of_car_accidents_smooth = map_dbl(seq_len(nrow(.)), spatial_smoothing))
 
 # plot
@@ -158,9 +158,9 @@ tm_shape(iow_bb, ext = 1.05) +
 # problems, again...
 
 # higher order?
-iow_graph_ego <- ego(iow_graph, order = 50)
+iow_graph_ego = ego(iow_graph, order = 50)
 
-iow_geofabric <- iow_geofabric %>%
+iow_geofabric = iow_geofabric %>%
   mutate(number_of_car_accidents_smooth = map_dbl(seq_len(nrow(.)), spatial_smoothing))
 
 # plot
