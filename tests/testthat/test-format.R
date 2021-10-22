b07a346ad8cefe830061c03908410b805fa8c86d
Robin Lovelace <rob00x@gmail.com>
2021-10-22 23:40:44 +0100

    Fix some tests

--- tests/testthat/test-format.R
+++ tests/testthat/test-format.R
@@ -13,7 +13,7 @@ context("test-format: vehicles")
 test_that("format_vehicles works", {
   skip_download()
   skip_on_cran()
-  fn = stats19::file_names$dftRoadSafetyData_Vehicles_2016.zip
+  fn = stats19::file_names$`dft-road-casualty-statistics-vehicle-2016.csv`
   dl_stats19(file_name = fn)
   # read it
   read = read_vehicles(
@@ -27,7 +27,7 @@ test_that("format_vehicles works", {
 context("test-format: casualties")
 
 test_that("format_casualties works", {
-  fn = stats19::file_names$dftRoadSafetyData_Casualties_2016.zip
+  fn = stats19::file_names$`dft-road-casualty-statistics-casualty-2016.csv`
   skip_download()
   skip_on_cran()
   dl_stats19(file_name = fn)
