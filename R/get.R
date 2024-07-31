360f2b26666fd896d61adae3d942ffffa95b8bef
robinlovelace <rob00x@gmail.com>
2024-07-31 12:29:08 +0100

    Set year to 1979 if earlier than 2018, close #239

--- R/get.R
+++ R/get.R
@@ -124,6 +124,11 @@ get_stats19 = function(year = NULL,
     output_format = "tibble"
   }
 
+  # Update year to 1979 if it's NULL or before 2018:
+  if (is.null(year) || year < 2018) {
+    year = 1979
+  }
+
   # download what the user wanted
   dl_stats19(year = year,
              type = type,
